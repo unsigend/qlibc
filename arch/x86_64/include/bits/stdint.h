ae55281523a9d7043198f4e5e06c767af4a5a74c
qiuyixiang <qiuyixiang2003@gmail.com>
2025-05-30 15:25:54 -0500

    add tests for stdint.h

---
+++ arch/x86_64/include/bits/stdint.h
@@ -0,0 +1,57 @@
+/* qlibc - A light-weight and portable C standard library
+ * Copyright (C) 2025 Qiu Yixiang
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+#ifndef _QLIBC_X86_64_BITS_STDINT_H_
+#define _QLIBC_X86_64_BITS_STDINT_H_
+
+typedef signed char         int8_t;
+typedef signed short        int16_t;
+typedef signed int          int32_t;
+typedef signed long long    int64_t;
+
+typedef int8_t              int_fast8_t;
+typedef int64_t             int_fast16_t;
+typedef int64_t             int_fast32_t;
+typedef int64_t             int_fast64_t;
+
+typedef int8_t              int_least8_t;
+typedef int16_t             int_least16_t;
+typedef int32_t             int_least32_t;
+typedef int64_t             int_least64_t;
+
+typedef signed long         intmax_t;
+typedef signed long         intptr_t;
+
+typedef unsigned char       uint8_t;
+typedef unsigned short      uint16_t;
+typedef unsigned int        uint32_t;
+typedef unsigned long long  uint64_t;
+
+typedef uint8_t             uint_fast8_t;
+typedef uint64_t            uint_fast16_t;
+typedef uint64_t            uint_fast32_t;
+typedef uint64_t            uint_fast64_t;
+
+typedef uint8_t             uint_least8_t;
+typedef uint16_t            uint_least16_t;
+typedef uint32_t            uint_least32_t;
+typedef uint64_t            uint_least64_t;
+
+typedef unsigned long long  uintmax_t;
+typedef unsigned long       uintptr_t;
+
+#endif
