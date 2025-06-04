901b052dad093b5359544f9747e4f44c5614f504
qiuyixiang <qiuyixiang2003@gmail.com>
2025-06-04 12:18:31 -0500

    implement <errno.h> for generic arch

---
+++ include/errno.h
@@ -0,0 +1,35 @@
+/* qlibc - A light-weight and portable C standard library
+ * Copyright (C) 2025 Qiu Yixiang
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+#ifndef _QLIBC_ERRNO_H
+#define _QLIBC_ERRNO_H
+
+// Include architecture-specific errno.h
+#include <sysdep/errno.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+extern int* __errno(void);
+#define errno (*__errno())
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
---
+++ src/errno/errno.c
@@ -0,0 +1,24 @@
+/* qlibc - A light-weight and portable C standard library
+ * Copyright (C) 2025 Qiu Yixiang
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+#include <errno.h>
+
+static int __errno_value = 0;
+
+int* __errno(void){
+    return &__errno_value;
+}
